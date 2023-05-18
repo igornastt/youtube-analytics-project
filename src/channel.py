6a197d54fa4cd583e9697fc1202318c046a46c73
igornast <grezle00@gmail.com>
2023-05-18 17:52:34 +0300

    Класс для ютуб-канала

--- src/channel.py
+++ src/channel.py
@@ -1,3 +1,14 @@
+import json
+import os
+
+# необходимо установить через: pip install google-api-python-client
+from googleapiclient.discovery import build
+
+
+api_key: str = os.getenv('YT_API_KEY')
+
+# создать специальный объект для работы с API
+youtube = build('youtube', 'v3', developerKey=api_key)
 
 
 class Channel:
@@ -5,8 +16,12 @@ class Channel:
 
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
-        pass
+        self.channel_id = channel_id
 
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
-        pass
+        channel = youtube.channels().list(id=self.channel_id, part='snippet,statistics').execute()
+        channel_info = json.dumps(channel, indent=2, ensure_ascii=False)
+        print(channel_info)
+
+
